f5eab7849ca09d6b24eb48845b04ee9c40869c86
AhmedAly01 <96546133+AhmedAly01@users.noreply.github.com>
2023-04-15 19:55:12 +0200

    frontend routh-hurwitz
    done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,17 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouthComponent } from './routh/routh.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     RouthComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/routh/routh.component.ts
+++ frontend/src/app/routh/routh.component.ts
@@ -6,18 +6,33 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./routh.component.css']
 })
 export class RouthComponent implements OnInit {
-  coefficients : any[] = [];
+  coefficients : any[] = [{}, {}, {}];
+  coefList : number[] = [0, 0, 0];
+  ansTable: number[][] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+  rootsInRHS: number = 0;
+  stabilityStatus: string = "Critically Stable";
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   addCoef() {
     this.coefficients.push({});
   }
 
+  removeCoef() {
+    this.coefficients.pop();
+  }
+
   submitCoef() {
-    console.log(this.coefficients);
+    this.coefList = this.coefficients.map(d => {return Number(d.value)});
+    console.log(this.coefList);
+    //here goes the http request to get the calculations from the back
+    //once we put the values into their respective variables, it will change on the UI
+    document.getElementById('ans')!.style.opacity = '1';
+  }
+
+  validateNo(e: KeyboardEvent) {
+    return (!isNaN(Number(e.key)) || e.key == "Backspace");
   }
 }
