7135d9cdcad1debea07e6653144ed6f8984114d2
Hesham-Youssef <hesham_ahmed2002002@yahoo.com>
2023-04-15 20:12:26 +0200

    MM

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { KonvaModule } from 'ng2-konva';
 
 import { AppComponent } from './app.component';
 
-import { RouthComponent } from './routh/routh.component';
+import { RouthComponent } from './components/routh/routh.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 
-import { RouthComponent } from './components/routh/routh.component';
-import {FormsModule} from "@angular/forms";
 import { GraphComponent } from './components/graph/graph.component';
-import { AppRoutingModule } from './app-routing.module';
+
 
 
 @NgModule({
