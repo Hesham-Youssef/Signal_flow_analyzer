b40083957099ced52426995029319b10466becd8
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-04-17 00:42:11 +0200

    back-work-flow

--- backend/src/main/java/com/example/backend/Controller.java
+++ backend/src/main/java/com/example/backend/Controller.java
@@ -3,7 +3,10 @@ package com.example.backend;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {}, allowedHeaders = "*")
 @RestController
@@ -11,8 +14,19 @@ import java.util.List;
 public class Controller {
     @GetMapping("/flowGraph")
     public JSONObject handleExampleRequest(@RequestParam("edges") List<List<Integer>> list, @RequestParam("nodes") int num) {
-        System.out.println(num);
-        System.out.println(list.toString());
-        return null;
+        List<Map<Integer, Double>> graph = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            graph.add(new HashMap<>());
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            graph.get(list.get(i).get(0)).put(list.get(i).get(1), Double.valueOf(list.get(i).get(2)));
+        }
+
+        Cycle_Detector detector = new Cycle_Detector(graph, 0, num-1);
+        JSONObject ans = detector.evaluateSystem();
+
+        System.out.println(ans);
+        return ans;
     }
 }
