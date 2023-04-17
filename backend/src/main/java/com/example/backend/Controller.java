80dadb72f9baf2d1876632bfd7af0a24bab4d41e
AhmedAly01 <96546133+AhmedAly01@users.noreply.github.com>
2023-04-17 05:23:23 +0200

    Controller got messed in last commit

--- backend/src/main/java/com/example/backend/Controller.java
+++ backend/src/main/java/com/example/backend/Controller.java
@@ -13,19 +13,28 @@ import java.util.Map;
 @RequestMapping("/solver")
 public class Controller {
     @GetMapping("/flowGraph")
-    public String handleExampleRequest(@RequestParam("edges") List<List<Integer>> list, @RequestParam("nodes") int num) {
+    public String flowGraphSolver(@RequestParam("edges") List<List<Double>> list, @RequestParam("nodes") int num) {
         List<Map<Integer, Double>> graph = new ArrayList<>();
         for (int i = 0; i < num; i++) {
             graph.add(new HashMap<>());
         }
 
-        for (List<Integer> integers : list) {
-            graph.get(integers.get(0)).put(integers.get(1), Double.valueOf(integers.get(2)));
+        for (int i = 0; i < list.size(); i++) {
+            graph.get(list.get(i).get(0).intValue()).put(list.get(i).get(1).intValue(), list.get(i).get(2));
         }
 
         Cycle_Detector detector = new Cycle_Detector(graph, 0, num-1);
         JSONObject ans = detector.evaluateSystem();
 
+        System.out.println(ans);
+        return ans.toString();
+    }
+
+    @GetMapping("/routhHurwitz")
+    public String routhHurwitzSolver(@RequestParam("edges") List<Double> list) {
+
+        Routh_Hurwitz routh = new Routh_Hurwitz(list);
+        JSONObject ans = routh.check_stability();
         System.out.println(ans);
         return ans.toString();
     }
