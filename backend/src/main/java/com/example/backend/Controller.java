831fd58a39cfcced9fbe883a1c7ff361c31862de
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-05-07 20:43:41 +0300

    done

--- backend/src/main/java/com/example/backend/Controller.java
+++ backend/src/main/java/com/example/backend/Controller.java
@@ -1,5 +1,6 @@
 package com.example.backend;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,32 +13,59 @@ public class Controller {
     @GetMapping("/flowGraph")
     public String handleExampleRequest(@RequestParam("edges") List<List<Double>> list, @RequestParam("nodes") int nodes, @RequestParam("start") String startNode, @RequestParam("end") String endNode) {
         List<Map<Integer, Double>> graph = new ArrayList<>();
-        int start = 0, end = nodes-1;
+        int start = 0, end = nodes-1, offset = 0;
         if(!startNode.equals("NaN")){
             start = Integer.parseInt(startNode);
+            offset = start;
+            start-= offset;
         }
         if(!endNode.equals("NaN")){
             end = Integer.parseInt(endNode);
+            end-=offset;
         }
-        if(start>end){
+       /* if(start>end){
             int x = end;
             end = start;
             start = x;
-        }
+        }*/
         for (int i = 0; i < end-start+1; i++) {
             graph.add(new HashMap<>());
         }
 
         for (List<Double> doubles : list) {
-            if(doubles.get(0).intValue()-start>=0 && doubles.get(1).intValue()<=end){
-                graph.get(doubles.get(0).intValue()-start).put(doubles.get(1).intValue()-start,doubles.get(2));
+            if(doubles.get(0).intValue()-offset >= 0 && doubles.get(1).intValue()-offset>=0 && doubles.get(1)-offset <= end && doubles.get(0).intValue()-offset <= end ){
+                graph.get(doubles.get(0).intValue()-offset).put(doubles.get(1).intValue()-offset,doubles.get(2));
             }
         }
 
         Cycle_Detector detector = new Cycle_Detector(graph, start, end);
         JSONObject ans = detector.evaluateSystem();
 
-        System.out.println(ans);
+        JSONArray loopsArray = ans.getJSONArray("Loops");
+        JSONArray pathArray = ans.getJSONArray("Paths");
+
+        JSONArray loops = new JSONArray();
+        JSONArray paths = new JSONArray();
+
+        for (int i = 0; i < loopsArray.length(); i++) {
+            JSONArray innerArray = loopsArray.getJSONArray(i);
+            JSONArray array = new JSONArray();
+            for (int j = 0; j < innerArray.length(); j++) {
+                array.put(innerArray.getInt(j) + offset);
+            }
+            loops.put(array);
+        }
+
+        for (int i = 0; i < pathArray.length(); i++) {
+            JSONArray innerArray = pathArray.getJSONArray(i);
+            JSONArray array = new JSONArray();
+            for (int j = 0; j < innerArray.length(); j++) {
+                array.put(innerArray.getInt(j) + offset);
+            }
+          paths.put(array);
+        }
+
+        ans.put("Loops",loops).put("Paths",paths);
         return ans.toString();
     }
 
